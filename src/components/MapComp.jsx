3b6c647a4bb2b2b9548932fc0747162ac61f6c14
vlarionov <vladvlarionov@gmail.com>
2025-11-26 00:57:18 -0800

    hopefully fixed leaflet marker issues

--- src/components/MapComp.jsx
+++ src/components/MapComp.jsx
@@ -1,5 +1,21 @@
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 
+// potential fix for marker not rendering
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+    import markerIcon from 'leaflet/dist/images/marker-icon.png';
+    import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+    // This is crucial to prevent Leaflet from trying to load images from default paths
+    delete (L.Icon.Default.prototype)._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: markerIcon2x,
+      iconUrl: markerIcon,
+      shadowUrl: markerShadow,
+    });
+
+// --------------------------------
+
 // change the view to current coordinates
 function SetViewOnClick({ coords }) {
   const map = useMap();
